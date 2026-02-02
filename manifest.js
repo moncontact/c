0bf818f50066f047957d1f6375719c47d8b33a81
moncontact <moncontact223@gmail.com>
2026-02-02 22:13:13 +0000

    Delete manifest.js

--- manifest.js
+++
@@ -1,9 +0,0 @@
-{
-  "name": "Terminale TSE Docs",
-  "short_name": "TSE Docs",
-  "start_url": "./index.html",
-  "display": "standalone",
-  "background_color": "#667eea",
-  "theme_color": "#667eea",
-  "orientation": "portrait"
-    }
