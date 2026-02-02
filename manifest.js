1e6b07cc3b4e10839afa440e6f5deeeece6d525c
moncontact <moncontact223@gmail.com>
2026-02-02 21:27:14 +0000

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,9 @@
+{
+  "name": "Terminale TSE Docs",
+  "short_name": "TSE Docs",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#667eea",
+  "theme_color": "#667eea",
+  "orientation": "portrait"
+    }
